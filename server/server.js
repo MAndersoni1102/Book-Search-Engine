c69374f210f45f57d81a98fd4e7e741ece7cadc8
Michael Anderson <mandersonisd@gmail.com>
2024-08-22 22:07:15 -0600

    Fixes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const cors = require("cors");
 const path = require("path");
-const { authMiddleware } = require("../Develop/server/utils/auth");
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require("../Develop/server/schemas");
-const db = require("../Develop/server/config/connection");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
